03121a703ec17dd02294d5786c51d3c85d6da290
puq2tera <puq2tera@gmail.com>
2024-09-25 23:43:37 -0700

    nextjs test

---
+++ mango-rpg-build-calculator-website/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/mango-rpg-build-calculator",
+    output: "export",
+    reactStrictMode: true,
+};  
+
+export default nextConfig;
