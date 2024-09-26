0af144f4f6ee7f990eb075718ea7eb45783a1672
puq2tera <puq2tera@gmail.com>
2024-09-25 23:59:26 -0700

    nextjs test commit

--- mango-rpg-build-calculator-website/next.config.mjs
+++ mango-rpg-build-calculator-website/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     basePath: "/mango-rpg-build-calculator",
     output: "export",
-    reactStrictMode: true,
+    images: {unoptimized: true},
 };  
 
 export default nextConfig;
