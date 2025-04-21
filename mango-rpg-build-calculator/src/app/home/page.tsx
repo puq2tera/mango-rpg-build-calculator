70659bfe62b21d1612839b93dea1816cf754e8ed
puq2tera <104032947+puq2tera@users.noreply.github.com>
2025-04-21 02:57:07 -0700

    Redo to be cleaner

---
+++ mango-rpg-build-calculator/src/app/home/page.tsx
@@ -0,0 +1,10 @@
+import Hello from "@/app/components/hello";
+
+export default function home() {
+    return (
+        <>
+            <h2 className="text-3xl"> Homepage</h2>
+            <Hello />
+        </>
+    )
+}
---
+++ mango-rpg-build-calculator/src/app/talents/page.tsx
@@ -0,0 +1,9 @@
+export default function talents() {
+    return (
+        <main>
+            <h2>
+                Talents Page
+            </h2>
+        </main>
+    )
+}
