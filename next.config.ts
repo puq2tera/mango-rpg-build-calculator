4eb01c12ff2ff9e7f3cc87f5c3077a202896a451
puq2tera <104032947+puq2tera@users.noreply.github.com>
2025-04-21 03:41:21 -0700

    fix config and build command

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'export',
+  images: {
+    unoptimized: true, // required if using next/image
+  },
+  trailingSlash: true, // optional: helps GitHub Pages routing
+}
 
 export default nextConfig;
