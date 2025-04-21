d427b0ab413e6fdf404e9be9c4eebc6ff93d9fba
puq2tera <104032947+puq2tera@users.noreply.github.com>
2025-04-21 04:07:09 -0700

    fix differences between local and deployed

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: "/mango-rpg-build-calculator",
   images: {
     unoptimized: true, // required if using next/image
   },
