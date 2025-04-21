7223197ee128cbbe0849ca5f64ddca8e87029f52
puq2tera <104032947+puq2tera@users.noreply.github.com>
2025-04-21 03:35:57 -0700

    remove old fix weird dynamic component issue

--- old/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: "/mango-rpg-build-calculator",
-    output: "export",
-    images: {unoptimized: true},
-};  
-
-export default nextConfig;
